4ec696cb99065323b8f26deb86b7a73841f71a59
127001p <122626765+127001p@users.noreply.github.com>
2024-08-28 20:25:24 +0530

    Create TempCalc.c
    This is c program which take user input and convert the units according to the user
    It can convert 'C' to 'F'
    and convert 'F' to 'C'

---
+++ TempCalc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    char units;
+    double temp;
+
+    printf("\nPlease enter your units: ");
+    scanf("%c", &units);
+
+    if (units == 'C' || units == 'c') {
+        printf("\nPlease enter your temperature in Celsius: ");
+        scanf("%lf", &temp);
+        printf("\nYour temperature in Fahrenheit  is: %.2lf", (9*temp)/5 + 32);
+    }
+    else if (units == 'F' || units == 'f') {
+        printf("\nPlease enter your temperature in Fahrenheit: ");
+        scanf("%lf", &temp);
+        printf("\nYour temperature in Celsius :%.2lf" , ((temp - 32) * 5) / 9);
+    }
+    else {
+        printf("\n %c is not the measurement of temperature" , units);
+    }
+}
